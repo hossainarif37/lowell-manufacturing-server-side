45ad16c8aa611d1f2365269ac12b247430f11d54
hossainarif37 <hossainarif4937@gmail.com>
2022-05-28 20:00:41 +0600

    database connected

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+
+
 
 // middleware
 app.use(cors())
 app.use(express.json())
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vhj24zl.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB Connected');
+    } finally {
+        //   await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
