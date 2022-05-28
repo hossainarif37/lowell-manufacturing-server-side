235ad6ed42d4b2d3b12365b1f070aa8151fad1e1
hossainarif37 <hossainarif4937@gmail.com>
2022-05-28 22:05:16 +0600

    create payment api

--- index.js
+++ index.js
@@ -33,6 +33,17 @@ async function run() {
             const result = await productCollection.findOne(query);
             res.send(result);
         });
+        app.post('/create-payment-intent', async (req, res) => {
+            const order = await req.body;
+            const amount = await order.totalCost || 500;
+            // console.log(amount)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: parseInt(amount),
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
 
     } finally {
         //   await client.close();
